5b71bd8bfe18ef34dd6c04dc01fab4607a566391
SfundoMhlungu <devadress21@gmail.com>
2025-02-10 22:39:56 +0200

    cleanup and jsdoc

--- index.js
+++ index.js
@@ -1,22 +1,27 @@
 
 import CreateBunny from "./lib/bunny.js";
-import { fileURLToPath } from 'url';
-import path from "path"
+// import { fileURLToPath } from 'url';
+// import path from "path"
 import DEFAULT_OPTS from "./lib/settings.js";
+// import DEFAULT_OPTS from "./lib/settings.js";
 
-DEFAULT_OPTS.cwd = path.dirname(fileURLToPath(import.meta.url))
+export const OPTS = DEFAULT_OPTS
+ 
 
+// const start = process.hrtime.bigint();
 
-const start = process.hrtime.bigint();
-
-CreateBunny({port: 3000, DEBUG: true})
-const end = process.hrtime.bigint();
+// CreateBunny({port: 3000, DEBUG: true})
+// const end = process.hrtime.bigint();
 
 
 // On process shutdown
-process.on('SIGINT', async () => {
-    console.log(`Operation latency: ${(end - start) / BigInt(1e6)} ms`);
-    const memory = process.memoryUsage();
-      console.log(` Memory Usage: ${(memory.heapUsed / 1024 / 1024).toFixed(2)} MB`)
-    process.exit();
-  });
+// process.on('SIGINT', async () => {
+//     console.log(`Operation latency: ${(end - start) / BigInt(1e6)} ms`);
+//     const memory = process.memoryUsage();
+//       console.log(` Memory Usage: ${(memory.heapUsed / 1024 / 1024).toFixed(2)} MB`)
+//     process.exit();
+//   });
+
+
+
+export default CreateBunny 
